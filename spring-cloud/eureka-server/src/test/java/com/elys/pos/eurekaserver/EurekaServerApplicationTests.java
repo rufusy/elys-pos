fb39d63bd52ed6d57318a29771feb2929abf1c7d
rufusy <idachirufus@gmail.com>
2024-11-27 18:01:42 +0300

    Bring in an edge server and a discovery service

--- spring-cloud/eureka-server/src/test/java/com/elys/pos/eurekaserver/EurekaServerApplicationTests.java
+++ spring-cloud/eureka-server/src/test/java/com/elys/pos/eurekaserver/EurekaServerApplicationTests.java
@@ -1,13 +1,44 @@
 package com.elys.pos.eurekaserver;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EurekaServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
+    @Value("${app.username}")
+    private String username;
+
+    @Value("${app.password}")
+    private String password;
+
+    @Test
+    void healthy() {
+        TestRestTemplate restTemplateWithAuth = testRestTemplate.withBasicAuth(username, password);
+
+        String expectedResponseBody = "{\"status\":\"UP\"}";
+        ResponseEntity<String> responseEntity = restTemplateWithAuth.getForEntity("/actuator/health", String.class);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(expectedResponseBody, responseEntity.getBody());
+    }
+
+    @Test
+    void catalogLoads() {
+        TestRestTemplate restTemplateWithAuth = testRestTemplate.withBasicAuth(username, password);
+
+        String expectedResponseBody = "{\"applications\":{\"versions__delta\":\"1\",\"apps__hashcode\":\"\",\"application\":[]}}";
+        ResponseEntity<String> responseEntity = restTemplateWithAuth.getForEntity("/eureka/apps", String.class);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(expectedResponseBody, responseEntity.getBody());
+    }
 }
