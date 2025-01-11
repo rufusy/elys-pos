382f4a43e04b8c75b18dae1f6ddf6d11b6ec2987
rufusy <idachirufus@gmail.com>
2025-01-11 13:02:50 +0300

    move swagger auth and token urls to env file. move keycloak proto to env file.

--- microservices/inventory/src/main/java/com/elys/pos/inventory/v1/config/JwtDecoderConfig.java
+++ microservices/inventory/src/main/java/com/elys/pos/inventory/v1/config/JwtDecoderConfig.java
@@ -1,27 +1,37 @@
 package com.elys.pos.inventory.v1.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
-import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 
 import java.util.List;
 
 @Configuration
 public class JwtDecoderConfig {
 
+    private final String expectedIssuer;
+    private final String jwkSetUri;
+
+    public JwtDecoderConfig(@Value("${app.auth-server-iss}") String expectedIssuer,
+                            @Value("${app.auth-server-jwks-uri}") String jwkSetUri) {
+        this.expectedIssuer = expectedIssuer;
+        this.jwkSetUri = jwkSetUri;
+    }
+
     @Bean(name = "customJwtDecoder")
     public ReactiveJwtDecoder jwtDecoder() {
-        String jwkSetUri = "http://keycloak:8080/realms/demo/protocol/openid-connect/certs";
 
         NimbusReactiveJwtDecoder jwtDecoder = NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri).build();
 
         // Create a custom OAuth2TokenValidator for issuer validation
         OAuth2TokenValidator<Jwt> issuerValidator = token -> {
-            String expectedIssuer = "http://localhost:8086/realms/demo";
             if (!token.getIssuer().toString().equals(expectedIssuer)) {
                 return OAuth2TokenValidatorResult.failure(new OAuth2Error(
                         "invalid_issuer",
--- spring-cloud/gateway/src/main/java/com/elys/pos/gateway/v1/config/JwtDecoderConfig.java
+++ spring-cloud/gateway/src/main/java/com/elys/pos/gateway/v1/config/JwtDecoderConfig.java
@@ -1,5 +1,6 @@
 package com.elys.pos.gateway.v1.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.core.OAuth2Error;
@@ -15,15 +16,22 @@ import java.util.List;
 @Configuration
 public class JwtDecoderConfig {
 
+    private final String expectedIssuer;
+    private final String jwkSetUri;
+
+    public JwtDecoderConfig(@Value("${app.auth-server-iss}") String expectedIssuer,
+                            @Value("${app.auth-server-jwks-uri}") String jwkSetUri) {
+        this.expectedIssuer = expectedIssuer;
+        this.jwkSetUri = jwkSetUri;
+    }
+
     @Bean(name = "customJwtDecoder")
     public ReactiveJwtDecoder jwtDecoder() {
-        String jwkSetUri = "http://keycloak:8080/realms/demo/protocol/openid-connect/certs";
 
         NimbusReactiveJwtDecoder jwtDecoder = NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri).build();
 
         // Create a custom OAuth2TokenValidator for issuer validation
         OAuth2TokenValidator<Jwt> issuerValidator = token -> {
-            String expectedIssuer = "http://localhost:8086/realms/demo";
             if (!token.getIssuer().toString().equals(expectedIssuer)) {
                 return OAuth2TokenValidatorResult.failure(new OAuth2Error(
                         "invalid_issuer",
